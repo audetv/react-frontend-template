913b12ddbdd93102f8098cbdc3a79a534205f995
audetv <algsound@yandex.ru>
2023-06-13 23:37:13 +0300

    Changed test

--- frontend/src/components/Welcome.test.js
+++ frontend/src/components/Welcome.test.js
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import Welcome from "./Welcome";
 
 test('renders welcome', () => {
-  const { getByText } = render(<Welcome />);
-  const h1Element = screen.getByText(/Frontend Template/i)
-  expect(h1Element).toBeInTheDocument()
+  render(<Welcome />);
+  const h1Element = screen.getByText(/Frontend Template/i);
+  expect(h1Element).toBeInTheDocument();
 });
